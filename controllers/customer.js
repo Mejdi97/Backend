f79dbd7d10229f1aae75696e1c9682f299e1e03d
Mejdi97 <mejdi.kchouk@esprit.tn>
2021-11-21 19:13:46 +0100

    Auth and jwt

---
+++ controllers/customer.js
@@ -0,0 +1,119 @@
+const Customer = require ('../models/Customer');
+var mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const express = require('express');
+const jwt = require ('jsonwebtoken');
+
+
+
+  //getting all 
+exports.getAllCustomer = async (req, res) => {
+    try {
+      const customers = await Customer.find()
+      res.json(customers)
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+  }
+
+
+//getting One
+exports.getOneCustomer = (req, res) => {
+    Customer.findById(mongoose.Types.ObjectId(req.params.id)
+   , (err, customer) => {
+     res.json(customer);
+ 
+     return;
+   });
+ }
+
+
+
+//create custumor
+exports.createCustomer = async (req, res) => {
+
+    console.log(req.file)
+    try {
+      
+      const hashedPAssword = await bcrypt.hash(req.body.password, 10)  
+      const customer = new Customer({
+      wallet_address: req.body.wallet_address,
+      name: req.body.name,
+      password:hashedPAssword,
+      bio: req.body.bio,
+      protfolio: req.body.protfolio,
+      social_media_accounts : req.body.social_media_accounts
+  
+    })
+    
+      const newCustomer = await customer.save();
+      res.status(201).json(newCustomer)
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  }
+
+
+
+
+  //update customer
+  exports.updateCustomer = async(req,res)=>{
+    try{
+  const updatedCustomer = await Customer.updateOne(
+    {_id:req.params.id}, 
+    { $set: {wallet_address:req.body.wallet_address,
+      password: req.body.password,
+   name:req.body.name,
+   protfolio:req.body.protfolio,
+   social_media_accounts :req.body.social_media_accounts
+  
+  }}
+  );
+  res.json(updatedCustomer);
+    }catch (err){
+      res.status(400).json({ message: err.message })
+    }
+  }
+
+  //delete customer
+  exports.deleteCustomer = async (req, res) => {
+        Customer.deleteOne({_id: req.params.id}).then(
+          () => {
+            res.status(200).json({
+              message: 'Deleted!'
+            });
+          }
+        ).catch(
+          (error) => {res.status(400).json({error: error});
+          }
+        );
+      }
+
+
+
+//login
+
+  exports.login = (req, res, next) => {
+    Customer.findOne({ wallet_address: req.body.wallet_address })
+  .then(Customer => {
+    if (!Customer) {
+      return res.status(401).json({ error: 'Customer not found  !' });
+    }
+    bcrypt.compare(req.body.password, Customer.password)
+      .then(valid => {
+        if (!valid) {
+          return res.status(401).json({ error: 'password incorrect !' });
+        }
+        res.status(200).json({
+            CustomerId: Customer._id,
+          token: jwt.sign(
+            { CustomerId: Customer._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' }
+          )
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
+  })
+  .catch(error => res.status(500).json({ error })) 
+}
--- routes/customers.js
+++ routes/customers.js
@@ -1,148 +1,31 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const customerController = require('../controllers/customer');
 const Customer = require('../models/Customer')
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt');
-
-
+const auth = require ('../middleware/auth');
 
 
 // Getting all
-router.get('/', async (req, res) => {
-  try {
-    const customers = await Customer.find()
-    res.json(customers)
-  } catch (err) {
-    res.status(500).json({ message: err.message })
-  }
-})
-
-
-
-
-async function getCustomer(req, res, next) {
-  let customerr
-  try {
-    customerr = await Customer.findById(mongoose.Types.ObjectId(req.params.id))
-    if (customerr == null) {
-      return res.status(404).json({ message: 'Cannot find Customer' })
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message })
-  }
-
-  res.Customer = Customer
-  next()
-}
-
-
+router.get('/',customerController.getAllCustomer );
 
 // Getting One
-router.get('/:id', (req, res) => {
-   Customer.findById(mongoose.Types.ObjectId(req.params.id)
-  , (err, customer) => {
-    console.log(customer);
-
-    return;
-  });
-});
-
+router.get('/:id',customerController.getOneCustomer);
 
 // Creating one
-router.post('/', async (req, res) => {
-
-  console.log(req.file)
-  try {
-    
-    const hashedPAssword = await bcrypt.hash(req.body.password, 10)  
-  
-  
-  const customer = new Customer({
-
-    wallet_address: req.body.wallet_address,
-    name: req.body.name,
-    password:hashedPAssword,
-    bio: req.body.bio,
-    protfolio: req.body.protfolio,
-    social_media_accounts : req.body.social_media_accounts
-
-  })
-  
-    const newCustomer = await customer.save();
-    res.status(201).json(newCustomer)
-  } catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-})
-/*// Updating One
-router.patch('/:id', getCustomer, async (req, res) => {
-  if (req.body.wallet_address != null) {
-    res.Customer.wallet_address = req.body.wallet_address
-  }
-  if (req.body.name != null) {
-    res.Customer.name = req.body.name
-  }
-  try {
-    //nahki l majuscule  leee khaleha edhiak hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh chouf trah behi
-    const updatedCustomer = await customer.save(function(){})
-    res.status(201).json(updatedCustomer)
-  } catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-})*/
-
+router.post('/',auth,customerController.createCustomer );
 
 //UPDATE2
-router.patch('/:id',async(req,res)=>{
-  try{
-const updatedCustomer = await Customer.updateOne(
-  {_id:req.params.id}, 
-  { $set: {wallet_address:req.body.wallet_address,
-    password: req.body.password,
- name:req.body.name,
- protfolio:req.body.protfolio,
- social_media_accounts :req.body.social_media_accounts
-
-}}
-);
-res.json(updatedCustomer);
-  }catch (err){
-    res.status(400).json({ message: err.message })
-  }
-});
-
+router.patch('/:id',auth,customerController.updateCustomer);
 
 // Deleting One
+router.delete('/:id', auth,customerController.deleteCustomer);
 
-router.delete('/:id', getCustomer ,async (req, res) => {
-  try {
-    await res.Customer.deleteOne()
-    res.json({ message: 'Deleted Customer' })
-  } catch (err) {
-    res.status(500).json({ message: err.message})
-  }
-})
-
-//LOGIN 
-router.post('/login', async (req, res) => {
-  const customers = await Customer.find()
-  customers.find(Customer => Customer.name === req.body.name)
-  if (Customer == null) {
-    return res.status(400).send('Cannot find Customer')
-  }
-  try {
-    if(await bcrypt.compare(req.body.password, Customer.password)) {
-    res.json({ message: 'Success' })
-
-    } else {
-      res.json({ message: 'Not Allowed' })
-    }
-  } catch {
-    res.status(500).send()
-  }
-})
+//login 
+router.post('/login',auth,customerController.login);
 
 
 
 
-module.exports = router
+
+module.exports = router;
